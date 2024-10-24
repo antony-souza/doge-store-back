ecb7befac91dc7d65e2fc07b43a12303468c0301
antony-souza <antonygustavo09@hotmail.com>
2024-10-24 19:20:15 -0300

    o

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,22 @@ import { AppModule } from "./app.module";
 import { Logger, ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT_1 || 4200;
 
   app.useGlobalPipes(new ValidationPipe());
+
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Accept");
+    next();
+  });
+
+  app.enableCors({
+    allowedHeaders: "*",
+    origin: "*",
+  });
 
   await app.listen(PORT);
   new Logger().log(`Server is Running: ${PORT}`, "SERVER");
