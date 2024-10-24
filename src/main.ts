14c9284df8d6e966410682d02cdaa720745cd8a1
antony-souza <antonygustavo09@hotmail.com>
2024-10-24 19:59:57 -0300

    teste

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,16 @@ import { Logger, ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    credentials: true,
+    origin: "https://doge-store-front.vercel.app",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  });
+
   const PORT = process.env.PORT_1 || 4200;
 
   app.useGlobalPipes(new ValidationPipe());
-
-  app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Accept");
-    next();
-  });
-
-  app.enableCors({
-    allowedHeaders: "*",
-    origin: "*",
-  });
 
   await app.listen(PORT);
   new Logger().log(`Server is Running: ${PORT}`, "SERVER");
