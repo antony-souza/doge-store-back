d210233b728050be282a60cf29abddae1dce719e
antony-souza <antonygustavo09@hotmail.com>
2024-10-25 16:59:54 -0300

    teste cors novamente peste

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,21 @@ import { Logger, ValidationPipe } from "@nestjs/common";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-    );
-    next();
+  // Configurando o CORS de forma mais restritiva
+  app.enableCors({
+    origin: [
+      "https://doge-store-front.vercel.app",
+      "https://antony-souza.online",
+    ],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type, Authorization",
+    credentials: true,
   });
+
+  const PORT = process.env.PORT_1 || 4200;
 
   app.useGlobalPipes(new ValidationPipe());
 
-  const PORT = process.env.PORT_1 || 4200;
   await app.listen(PORT);
   new Logger().log(`Server is Running: ${PORT}`, "SERVER");
 }
