6836f9d329c5e432ba31685fb98a9f94792d2d9e
antony-souza <antonygustavo09@hotmail.com>
2024-11-04 20:18:13 -0300

    teste

--- src/module/store/store.service.ts
+++ src/module/store/store.service.ts
@@ -136,6 +136,8 @@ export class StoreService {
 
     let url = existingStore.image_url;
 
+    console.log("teste");
+
     if (updateStoreDto.image_url) {
       url = await this.uploadFilService.upload(updateStoreDto.image_url[0]);
     }
