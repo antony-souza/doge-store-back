bd01c5de238557087278075cbcc2756e6031ee04
antony-souza <antonygustavo09@hotmail.com>
2024-11-26 10:43:40 -0300

    Passando environment no init-user

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -6,14 +6,8 @@ class Environment {
   public readonly port: number = parseInt(process.env.PORT_1 ?? "");
 
   //Redis
-<<<<<<< HEAD
-  // eslint-disable-next-line prettier/prettier
   public readonly redisCacheHost: string = process.env.REDIS_CACHE_HOST ?? "";
   // eslint-disable-next-line prettier/prettier
-=======
-  public readonly redisCacheHost: string = process.env.REDIS_CACHE_HOST ?? "";
-  // eslint-disable-next-line prettier/prettier
->>>>>>> dev
   public readonly redisCachePort: number = parseInt(process.env.REDIS_CACHE_PORT ?? "");
   // eslint-disable-next-line prettier/prettier
   public readonly redisCachePassword: string = process.env.REDIS_CACHE_PASSWORD ?? "";
